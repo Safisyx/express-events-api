97c589dc8a29b0c94c24eef754ac8f1dd55bcc0a
Safisyx <safisyx@gmail.com>
2018-03-04 17:55:07 +0100

    Set up database

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const app = express()
 
+var Sequelize = require('sequelize')
+var sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres')
+
+const MyEvent = sequelize.define('myEvent', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  start_date: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  end_date: {
+    type: Sequelize.DATE
+  },
+  description: Sequelize.STRING
+}, {
+  tableName: 'events',
+  timestamps: false,
+  //timezone:+0100
+})
 
 
 app.listen(4001, () => console.log('Express API listening on port 4001'))
@@ -10,3 +31,5 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
   next()
 })
+
+MyEvent.findById(1).then(myEvent => console.log(JSON.stringify(myEvent)))
