cc4fddd1946dc5252da350eafdc0411118487e30
Safisyx <safisyx@gmail.com>
2018-03-04 20:06:32 +0100

    Allow to update event

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const MyEvent = sequelize.define('myEvent', {
     allowNull: false
   },
   start_date: {
-    type: Sequelize.DATE,
+    type: Sequelize.DATEONLY,
     allowNull: false
   },
   end_date: {
-    type: Sequelize.DATE
+    type: Sequelize.DATEONLY
   },
   description: Sequelize.STRING
 }, {
@@ -66,16 +66,24 @@ app.get('/events/:id', (req, res) => {
     })
 })
 
+const endDateProblem = (res) => {
+  res.status(500)
+  res.json({ message: "Problem, ending date cannot happen before the starting date!!"})
+}
+
+const cannotGoBackInTime = (res) => {
+  res.status(501)
+  res.json({ message: "Cannot go back in time!!"})
+}
+
 app.post('/events', (req, res) => {
   const myEvent = req.body
   //console.log(Date.now())
   if (myEvent.end_date<myEvent.start_date){
-    res.status(500)
-    res.json({ message: "Problem, ending date cannot happen before the starting date!!"})
+    endDateProblem(res)
   }
   else if (myEvent.start_date< new Date().toISOString().slice(0,10)) {
-    res.status(501)
-    res.json({ message: "Cannot go back in time!!"})
+    cannotGoBackInTime(res)
   }
   else {
     MyEvent.create(myEvent)
@@ -89,3 +97,71 @@ app.post('/events', (req, res) => {
       })
   }
 })
+
+const updateOrPatch = (req, res) => {
+  //const productId = Number(req.params.id)
+  const updates = req.body
+
+  // find the product in the DB
+  MyEvent.findById(req.params.id)
+    .then(entity => {
+      // change the product and store in DB
+      const eEnd = entity.end_date
+      const eStart = entity.start_date
+      const uStart = updates.start_date
+      const uEnd = updates.end_date
+
+      if (uStart && uEnd) {
+        if (uStart>uEnd) {
+          endDateProblem(res)
+        }
+        else if (uStart < new Date().toISOString().slice(0,10)) {
+          cannotGoBackInTime(res)
+        }
+        else{
+          return entity.update(updates)
+        }
+      }
+
+      if (uStart && !uEnd) {
+        if (uStart>uEnd) {
+          endDateProblem(res)
+        }
+        else if (uStart < new Date().toISOString().slice(0,10)) {
+          cannotGoBackInTime(res)
+        }
+        else{
+          return entity.update(updates)
+        }
+      }
+
+      if (!uStart && uEnd) {
+        if (eStart>uEnd) {
+          endDateProblem(res)
+        }
+        else if (uEnd < new Date().toISOString().slice(0,10)) {
+          cannotGoBackInTime(res)
+        }
+        else{
+          return entity.update(updates)
+        }
+      }
+
+      if (!uStart && !uEnd) {
+        return entity.update(updates)
+      }
+    })
+    .then(final => {
+      // respond with the changed product and status code 200 OK
+      res.send(final)
+    })
+    .catch(error => {
+      res.status(500).send({
+        message: `Something went wrong`,
+        error
+      })
+    })
+}
+
+app.put('/events/:id', updateOrPatch)
+app.patch('/events/:id', updateOrPatch)
